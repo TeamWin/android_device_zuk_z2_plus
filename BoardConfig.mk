1c3a8661ab1467fe28c9431df800daa79eb2c243
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    z2_plus: Use libcryptfs_hw from commonsys repo
    Change-Id: I342f57e9e66cd4f6674d7ce318a2a8dabebef8c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,7 @@ TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
 
 # Encryption
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Partitions
